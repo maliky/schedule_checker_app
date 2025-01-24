ff721bb67de96d1c1f0ef42846722456c06bd5a2
mlk <malik@kone.ci>
2025-01-24 13:19:43 +0000

    fine tuning the app runing script

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-export PYTHONPATH=$(pwd)
-flask run
+
+# Check the environment variable FLASK_ENV
+if [ "$FLASK_ENV" == "development" ]; then
+    echo "Running in development mode..."
+    flask run --host=127.0.0.1 --port=5000
+else
+    echo "Running in production mode..."
+    gunicorn --bind 0.0.0.0:9090 --workers 4 --threads 2 online_schedule_checker:app
+fi
